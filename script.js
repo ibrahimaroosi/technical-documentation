08630986445e20db3939aa4cef7fee1eb18de4df
Prince Ibrahim <61180254+ibrahimaroosi@users.noreply.github.com>
2021-06-03 23:40:44 +0530

    new upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// rotate();
+// function rotate() {
+//     document.getElementById("rotate").style.msTtransform = "rotate(20deg)";
+//     document.getElementById("rotate").style.ttransform = "rotate(20deg)";
+// }
