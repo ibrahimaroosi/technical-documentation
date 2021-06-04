c00e7151f82cba09b479f465438c845fffcd72d0
Prince Ibrahim <61180254+ibrahimaroosi@users.noreply.github.com>
2021-06-04 12:47:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// rotate();
-// function rotate() {
-//     document.getElementById("rotate").style.msTtransform = "rotate(20deg)";
-//     document.getElementById("rotate").style.ttransform = "rotate(20deg)";
-// }
